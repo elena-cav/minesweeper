3dfbef14e30b60d0a1cd5e3c939a72d211910381
Elena Cavallero <elecav@hotmail.it>
2022-10-11 15:43:19 +0100

    first commit

---
+++ utils/README.md
@@ -0,0 +1,16 @@
+# Challenge 13 - “Minesweeper”
+
+Before we take a break from grid-based challenges, your dastardly dare for this week is to recreate the logic behind the classic Minesweeper game. Given a grid of characters where each X represents a mine and each - represents empty space, the aim is to return a grid where each - is replaced by an integer indicating the number of mines adjacent to that spot (including diagonally).
+Rewards:
+
+- five: Points are awarded for a working solution.
+- three: Further points are awarded for creating a playable game (i.e. a working GUI).
+- two: Further points are awarded for creating more than one difficulty level in your playable game.
+
+Example:
+
+`minesweeper([ "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "X", "X", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "X", ])`
+
+would return:
+
+`[ "0", "0", "0", "0", "0", "2", "2", "1", "0", "0", "X", "X", "1", "0", "0", "2", "2", "1", "1", "1", "0", "0", "0", "1", "X", ]`
---
+++ utils/index.ts
@@ -0,0 +1,37 @@
+const chunkArray = (myArray: string[], chunk_size: number): string[][] => {
+  const results: string[][] = [];
+  while (myArray.length) {
+    results.push(myArray.splice(0, chunk_size));
+  }
+  return results;
+};
+
+export default (grid: string[]): string[] => {
+  const chunked = chunkArray(grid, 5);
+  const pointed = chunked
+    .map((chunk: string[], i: number): string[] =>
+      chunk.map((cell: string, index: number): string => {
+        const adjacentCell = [
+          chunk[index - 1] || "",
+          chunk[index + 1] || "",
+          chunked[i - 1]?.[index] || "",
+          chunked[i + 1]?.[index] || "",
+          chunked[i - 1]?.[index - 1] || "",
+          chunked[i - 1]?.[index + 1] || "",
+          chunked[i + 1]?.[index - 1] || "",
+          chunked[i + 1]?.[index + 1] || "",
+        ];
+        let score: string | number = cell;
+        if (cell === "-") {
+          score = adjacentCell.reduce(
+            (acc: number, curr: string): number =>
+              curr === "X" ? acc + 1 : acc,
+            0
+          );
+        }
+        return score.toString();
+      })
+    )
+    .flat();
+  return pointed;
+};
